57acf1c601519c0a6bc694d318d9812af93f27c1
dave caruso <dave@davecode.me>
2020-04-22 23:46:40 -0400

    remove unneded type castings

--- src/util/structures/command/RunArguments.ts
+++ src/util/structures/command/RunArguments.ts
@@ -4,8 +4,8 @@ export const RunArguments = (message: Message, args: string[]) => {
   return {
     args,
     guild: message.guild,
-    message: message as Message,
-    msg: message as Message,
+    message,
+    msg: message,
     user: message.author,
   } as RunArgumentsOptions;
 };
