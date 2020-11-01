85fd0634133a476001d70eb2221494516e3123c1
Lucks <lucks@outlook.pt>
2020-11-01 22:20:47 +0000

    ignore settings

--- src/settings.ts
+++ src/settings.ts
@@ -1,13 +1,13 @@
 export default {
   channels: {
-    welcome: '701865087442616380',
-    rules: '701865087442616380',
-    roles: '701865087442616383',
-    logJoins: '712364654877474846',
-    logInfractions: '712364674892693534',
-    logMessages: '712381454209908777',
-    challenges: '764967203714891777',
-    challengeWinners: '764990166791094285',
+    welcome: '479298119255851029',
+    rules: '479298119255851029',
+    roles: '701031262558879814',
+    logJoins: '715988890359431168',
+    logInfractions: '715988890359431168',
+    logMessages: '715988890359431168',
+    challenges: '770740089721847848',
+    challengeWinners: '770740272425861130',
   },
   colors: {
     info: 0x6495ed,
@@ -19,21 +19,21 @@ export default {
   },
   developers: ['244905301059436545', '131857875973701633', '429983129739067413'], // dave, lucks, tim
   emotes: {
-    challengeVote: '765213656681676810',
+    challengeVote: '701042202373259324',
   },
   messages: {
-    rules: '702665704930934835',
-    levelUp: "Congratulations %mention%! You're now level %level%.",
-    raidKickMessage: 'You have been automatically kicked from Blackmagic Community since the server is in lockdown. Try again later.',
-    challengeVoting: 'Voting phase has begun! React with :thumbsup: or :thumbsdown: to your favourite!',
-    challengeStart: 'This challenge is titled `%title%`. We want stuff that meets the `%topic%` topic\n\n%desc%\n\nRemember - have fun!',
+    rules: '624188483308879872',
+    levelUp: "Congratulations %mention%! You're now level %level%.\n*You can opt-out of these messages in the roles menue*",
+    raidKickMessage: 'You have been automatically kicked from Blackmagic Community since the server is in lockdown. Try again later please.',
+    challengeVoting: 'Voting phase has begun! React with :bmd: to your favourite!',
+    challengeStart: 'New challenge! `%topic%`\n\n%desc%\n\nYou have time to post your entry till saturday when the voting starts. Have fun!',
     challengeWinner:
       '**%title%** (%topic%) challenge voting phase has ended and the winner is (drumroll please)....\n\n%mention% with %votes% votes!!!!\nCheck out what they made by clicking on %messageLink%!', // %messageLink% is replaced with [here](url)
     challengeWinners:
       '**%title%** (%topic%) challenge voting phase has ended and we got a... tie!!! The winners are (drumroll please).... \n\n%mentions% with %votes% votes!!!!\nCheck out what they made by clicking on %messageLinks%!', // %messageLinks% is replaced with [Username#discrim](url), [Username#discrim](url2)
   },
   owner: '131857875973701633',
-  prefixes: ['&', '!bmd '],
+  prefixes: ['!','!bmd','!bmd '],
   raid: {
     memberJoinInterval: 10000, // in ms
     okWait: 1200000, // in ms
@@ -42,7 +42,7 @@ export default {
     okVerificationLevel: 'LOW',
   },
   roles: {
-    challenge: '764975817280847873',
-    private: '761336867223896085',
+    challenge: '770742440490565673',
+    private: '770398819447341096',
   },
 };
