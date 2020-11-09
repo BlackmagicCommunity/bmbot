423c2b3acfdfc929574cc471049a65195425c990
Tim Haettich <33052519+timhaettich@users.noreply.github.com>
2020-11-09 18:35:42 +0100

    Move say command here

---
+++ src/commands/moderation/say.ts
@@ -0,0 +1,36 @@
+import { TextChannel } from 'discord.js';
+import { Client, Command, RunArgumentsOptions } from '../../util';
+
+export default class SayCommand extends Command {
+  constructor(client: Client) {
+    super(client, {
+      help: 'Replicates your message.',
+      deletable: true,
+      requiredPermissions: ['BAN_MEMBERS'],
+      arguments: [
+        {
+          name: 'channel',
+          required: true,
+          type: 'Channel',
+        },
+        {
+          name: 'message',
+          required: true,
+          type: 'string',
+        },
+      ],
+    });
+  }
+
+  public async main({ msg, args }: RunArgumentsOptions) {
+    let channel = (await this.client.util.getChannel(msg, args[0], true)) as TextChannel;
+    if (!channel) channel = msg.channel as TextChannel;
+    else {
+      try {
+        await channel.send(args[1]);
+      } catch {
+        msg.channel.send(`:x: Can't send messages there.`);
+      }
+    }
+  }
+}
