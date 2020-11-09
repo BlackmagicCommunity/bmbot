c646918b51cf5ac82abd3dbdd4bc069adad36150
Tim Haettich <33052519+timhaettich@users.noreply.github.com>
2020-11-09 18:35:13 +0100

    Add rtfm Command

---
+++ src/commands/fun/rtfm.ts
@@ -0,0 +1,29 @@
+import { TextChannel } from 'discord.js';
+import { Client, Command, RunArgumentsOptions } from '../../util';
+
+export default class RTFMCommand extends Command {
+  constructor(client: Client) {
+    super(client, {
+      aliases: ['readthefuckingmanual','rtm'],
+      help: 'If someone asks a question that is easily findable in the manual',
+      deletable: true,
+      arguments: [
+        {
+          name: 'query | link',
+          type: 'string | url',
+        },
+      ],
+    });
+  }
+
+  public async main({ msg, args }: RunArgumentsOptions) {
+    msg.channel.send(
+      randomOf([
+        'https://sd.keepcalms.com/i/keep-calm-and-read-the-fucking-manual-2.png',
+        'https://static.giga.de/wp-content/uploads/2017/02/RTFM-Meme.jpg',
+        'https://youtu.be/Z1VKn_TIXFM?t=26',
+        'Read the fucking manual!\n https://images.squarespace-cdn.com/content/v1/54bcbd06e4b060f2e987ebbe/1459530149424-43S9A8RI1DN2JYXJOQ64/ke17ZwdGBToddI8pDm48kIjqXxgTUt6jL4bCHLahW0VZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpzI3ktOOqlYFVERKzP1A3uKN-hQOFf7CmjFb1WHA8IvWYefrUROdDUSV9FRYfXBRr0/image-asset.png',
+      ])
+    )
+  }
+}
